fd8c651b1f5759ead758c87b87b37ca0cf99d9cc
DESKTOP-4CDNPFM\Egor <luk_96@mail.ru>
2022-04-07 21:05:05 +0300

    add GameHistoryRepository

---
+++ DomainLogic/Repositories/IGameHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using DomainLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLogic.Repositories
+{
+    public interface IGameHistoryRepository
+    {
+        Task SaveBoardState(BoardState boardState);
+        Task<BoardState> GetLastBoardState(Guid gameId);
+    }
+}
---
+++ Implementations.Repositories/GameHistoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using DomainLogic.Models;
+using DomainLogic.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Implementations.Repositories
+{
+    public class GameHistoryRepository : IGameHistoryRepository
+    {
+        private static List<BoardState> boardStates = new List<BoardState>();
+
+        public async Task<BoardState> GetLastBoardState(Guid gameId)
+        {
+            return boardStates
+                .Where(b => b.GameId == gameId)
+                .OrderByDescending(b => b.CreateDateTime)
+                .FirstOrDefault();
+        }
+
+        public async Task SaveBoardState(BoardState boardState)
+        {
+            boardStates.Add(boardState);
+        }
+    }
+}

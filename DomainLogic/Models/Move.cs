54106907b03264ebc35be385daa94eec3af2b9b9
DESKTOP-4CDNPFM\Egor <luk_96@mail.ru>
2022-04-25 21:15:58 +0300

    rename PossibleMove to Move

--- DomainLogic/Models/Move.cs
+++ DomainLogic/Models/Move.cs
@@ -2,7 +2,7 @@
 
 namespace DomainLogic.Models
 {
-    public record PossibleMove(
+    public record Move(
         CellCoordinate From,
         CellCoordinate To,
         CellCoordinate CapturableCheckerCoordinate = null

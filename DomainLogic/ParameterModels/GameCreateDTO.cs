429307dd0a00cbe04936fd94722420a47692f861
DESKTOP-4CDNPFM\Egor <luk_96@mail.ru>
2022-08-01 15:55:33 +0300

    update GameCreateDTO

--- DomainLogic/ParameterModels/GameCreateDTO.cs
+++ DomainLogic/ParameterModels/GameCreateDTO.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace DomainLogic.ParameterModels
 {
     public record GameCreateDTO(
-        string MyCeckerColor = "",
-        string OpponentCheckerColor = "",
-        string CheckerCellColor = "",
-        string NonPlayableCellColor = "");
+        string MyCeckerColor = "#000000",
+        string OpponentCheckerColor = "#FFFFFF",
+        string CheckerCellColor = "#000000",
+        string NonPlayableCellColor = "#FFFFFF");
 }
